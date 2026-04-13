a6e32e76c1079df1c67a4654c1d668f817c0df9c
eseniav <esenia-v@yandex.ru>
2026-04-13 13:28:29 +0300

    add validation message

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
+    res.redirect("auth.html");
+})
+
+app.post("/register", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    if(!data.login || !data.password)
+        return res.status(400).send("Заполните поля логин и пароль");
     res.redirect("auth.html");
 })
 
