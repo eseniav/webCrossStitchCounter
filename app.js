6144cfce799345ffafdaab785c85be3468187a7a
eseniav <esenia-v@yandex.ru>
2026-04-13 14:22:38 +0300

    add data user to file

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
+const USERS_FILE = path.join(__dirname, "data", "users.json");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
@@ -11,10 +13,29 @@ app.get("/", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const data = req.body;
-    console.log(data);
-    if(!data.login || !data.password)
+    const regUser = req.body;
+    delete regUser.repeatPassword;
+    console.log(regUser);
+    if(!regUser.login || !regUser.password)
         return res.status(400).send("Заполните поля логин и пароль");
+    let users;
+    try {
+        const data = fs.readFileSync(USERS_FILE, "utf-8");
+        users = JSON.parse(data);
+    } catch(error)
+    {
+        if(error.code === "ENOENT")
+            users = [];
+        else
+            throw error;
+    }
+    const exists = users.find((item) => item.login == regUser.login);
+    if(exists)
+        return res.status(409).send("Пользователь с таким логином уже существует");
+    
+    users.push(regUser);
+    fs.mkdirSync(path.dirname(USERS_FILE), {recursive: true});
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2), "utf-8");
     res.redirect("auth.html");
 })
 
