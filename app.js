b795d6f4746e3468199abe09dbaf288c1e03ab28
eseniav <esenia-v@yandex.ru>
2026-04-13 12:57:53 +0300

    redirect auth file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.send("app is running");
+    res.redirect("auth.html");
 })
 
 app.listen(3000, () => {
