8637fde10082ab91b4373bf9a69229e048399d6d
eseniav <esenia-v@yandex.ru>
2026-04-13 12:38:13 +0300

    add npm script

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("app is running");
+})
+
 app.listen(3000, () => {
     console.log("server running on http://localhost:3000");
 })
