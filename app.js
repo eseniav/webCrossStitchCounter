fe495202df06ded514ae9a0f69064f19a0e1c14e
eseniav <esenia-v@yandex.ru>
2026-04-14 13:15:26 +0300

    extract set, get, login users to functions

--- app.js
+++ app.js
@@ -18,41 +18,53 @@ app.post("/register", (req, res) => {
     console.log(regUser);
     if(!regUser.login || !regUser.password)
         return res.status(400).send("Заполните поля логин и пароль");
-    let users;
-    try {
-        const data = fs.readFileSync(USERS_FILE, "utf-8");
-        users = JSON.parse(data);
-    } catch(error)
-    {
-        if(error.code === "ENOENT")
-            users = [];
-        else
-            throw error;
-    }
-    const exists = users.find((item) => item.login == regUser.login);
-    if(exists)
+    users = getUsers();
+    if(checkUserExist(regUser))
         return res.status(409).send("Пользователь с таким логином уже существует");
     
     users.push(regUser);
-    fs.mkdirSync(path.dirname(USERS_FILE), {recursive: true});
-    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2), "utf-8");
+    setUsers(users);
     res.redirect("auth.html");
 })
 
+function setUsers(users) {
+    fs.mkdirSync(path.dirname(USERS_FILE), {recursive: true});
+    fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2), "utf-8");
+}
+
+function getUsers() {
+    try {
+        const data = fs.readFileSync(USERS_FILE, "utf-8");
+        return JSON.parse(data);
+    } catch(error)
+    {
+        if(error.code === "ENOENT")
+            return [];
+        else
+            throw error;
+    }
+}
+
+function checkUserExist(user) {
+    return getUsers().find((item) => item.login == user.login);
+}
+
+function loginUser(user) {
+    const findUser = checkUserExist(user);
+    if(!findUser)
+        return null;
+    if(findUser.password != user.password)
+        return false;
+    return findUser;
+    
+}
 app.post("/auth", (req, res) => {
     const regUser = req.body;
     if(!regUser.login || !regUser.password)
         return res.status(400).send("Заполните поля логин и пароль");
-    let users;
-    try {
-        const data = fs.readFileSync(USERS_FILE, "utf-8");
-        users = JSON.parse(data);
-    } catch(error)
-    {
-        throw error;
-    }
-    const respond = users.find((item) => item.login == regUser.login && item.password == regUser.password);
-    if(!respond)
+    if(loginUser(regUser) == null)
+        return res.status(401).send("Пользователь не найден");
+    if(!loginUser(regUser))
         return res.status(401).send("Ошибка авторизации. Проверьте правильность заполнения полей");
     res.redirect("gallery.html");
 })
